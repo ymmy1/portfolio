791533823f06d5fbcc0f9da8a71c1795702a45e8
ymmy1 <chem-pion@mail.ru>
2019-09-17 16:52:16 -0700

    changing font style

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-jQuery(document).ready(function($){
-	
-	$('a.contact-me').click(function(e){
-		e.preventDefault();
-		$id = $(this).attr('href');
-		$('.email a').removeClass("blink");
-		$('body,html').animate({
-			scrollTop: $($id).offset().top -20
-		}, 750);
-		$('.email a').addClass("blink");
-	});
+jQuery(document).ready(function($) {
 
-		$('a.imgcontact').click(function(e){
-		e.preventDefault();
-		$id = $(this).attr('href');
-		$('body,html').animate({
-			scrollTop: $($id).offset().top -20
-		}, 750);
-	});
+    $('a.contact-me').click(function(e) {
+        e.preventDefault();
+        $id = $(this).attr('href');
+        $('.email a').removeClass("blink");
+        $('body,html').animate({
+            scrollTop: $($id).offset().top - 20
+        }, 750);
+        $('.email a').addClass("blink");
+    });
+
+    $('a.imgcontact').click(function(e) {
+        e.preventDefault();
+        $id = $(this).attr('href');
+        $('body,html').animate({
+            scrollTop: $($id).offset().top - 20
+        }, 750);
+    });
 });
