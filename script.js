7366aacec18dd0eba9c95cd5d3be10fbdbc1208b
ymmy1 <chem-pion@mail.ru>
2020-09-05 15:48:30 -0700

    animations for title 2/3

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Wrap every letter in a span
+$( document ).ready(function() {
+    
+    goodMorningTitle();
+    setTimeout(function(){ frontEndTitle()}, 3000);
+});
+
 function sendMail() {
     var link = "mailto:oleg.ymmy@gmail.com"
              + "?cc=chem-pion@mail.ru"
@@ -6,4 +13,48 @@ function sendMail() {
     ;
     
     window.location.href = link;
+}
+
+// Good Morning Animate\
+function goodMorningTitle()
+{
+    var textWrapper = document.querySelector('.ml3');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    
+        anime.timeline({loop: false})
+        .add({
+            targets: '.ml3 .letter',
+            opacity: [0,1],
+            easing: "easeInOutQuad",
+            duration: 250,
+            delay: (el, i) => 50 * (i+1)
+        }).add({
+            targets: '.ml3',
+            opacity: 1,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+}
+
+// Front End Developer Animate
+function frontEndTitle()
+{
+    var textWrapper = document.querySelector('.ml5');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    textWrapper.style.opacity = "1"
+    anime.timeline({loop: false})
+    .add({
+        targets: '.ml5 .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 250,
+        delay: (el, i) => 50 * (i+1)
+    }).add({
+        targets: '.ml5',
+        opacity: 1,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
 }
